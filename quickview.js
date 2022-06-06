cd3c146bb1c0292dd451a548e8ca24f00a4cd6b0
invintsLLP <87797457+invintsLLP@users.noreply.github.com>
2022-06-06 15:18:04 +0530

    Update quickview.js

--- quickview.js
+++ quickview.js
@@ -9,22 +9,29 @@ quickview.forEach(function(i) {
     var url_location = location.origin;
     // Url for the request 
     var url = url_location +'/products/'+product_handle+'.json';
-
+    console.log(url);
     // Making our request 
     fetch(url, { method: 'GET' })
     .then(Result => Result.json())
     .then(string => {
 
-      // Printing our response
+      // Printing our response 
+      console.log(string);
       var quick_product = string.product;
       var app = new Vue({
         el: '#quickview_viewer',
         data: {
-          json:quick_product,
-          id:quick_product.id
+          json:quick_product
+        },
+        methods: {
+          onChange(event) {
+            var value= event.target.value
+            document.querySelector('#quickview_viewer input[name="id"]').setAttribute('value', value);
+          }
         }
       })
       document.querySelector('#quickview_viewer').classList.add('open-quickview');
+
     })
     .catch(errorMsg => { console.log(errorMsg); });
   });
